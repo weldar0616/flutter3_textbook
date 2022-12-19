b332962312a14aeb33290a6bbf76921648d51722
weldar0616 <weldar0509@gmail.com>
2022-12-19 17:57:48 +0900

    2-2 Stateクラスの利用

--- lib/main.dart
+++ lib/main.dart
@@ -117,22 +117,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  final title = 'Flutterサンプル';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-        ),
-        body: Text(
-          'Hello Flutter World!',
-          style: TextStyle(fontSize: 32.0),
-        ),
+        title: 'Flutter Demo',
+        home: MyHomePage(
+          title: this.title,
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    required this.title,
+  }) : super();
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class Data {
+  int _price;
+  String _name;
+  Data(this._name, this._price) : super();
+
+  @override
+  String toString() {
+    return _name + ':' + _price.toString() + '円';
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static final _data = [
+    Data('Apple', 200),
+    Data('Orange', 150),
+    Data('Peach', 300)
+  ];
+  Data _item = _data[0];
+
+  void _setData() {
+    setState(() {
+      _data.shuffle();
+      _item = _data.first;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Set data'),
+      ),
+      body: Text(
+        _item.toString(),
+        style: TextStyle(fontSize: 32.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setData,
+        tooltip: 'set data.',
+        child: Icon(Icons.star),
       ),
     );
   }
